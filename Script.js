50228e95b1343030c9fd8e0389a76c70d4852feb
MrSkidood <sbotnari6@gmail.com>
2023-01-24 22:35:49 +0200

    Added a way to make a username and check if it is empty or not

--- Script.js
+++ Script.js
@@ -1,1 +1,19 @@
 "use strict";
+usernameInput();
+
+
+function usernameInput() {
+    let username = prompt("Enter username:")
+    if (!username) {
+        alert("Username cannot be null!")
+        usernameInput();
+    }
+    else {
+        const USERNAME = username;
+        alert(`Welcome ${USERNAME}`)
+    }
+}
+function main() {
+
+}
+    

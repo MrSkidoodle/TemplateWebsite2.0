4fc4e8b7844b7e61f03f4a32a0bc9bd5f1e479cd
stefan botnari <theblindfold123@gmail.com>
2023-01-25 12:57:33 +0200

    fixed the is even or odd function

--- Script.js
+++ Script.js
@@ -3,9 +3,11 @@ even();
 function even() {
     let evenOdd = prompt("type a number");
     if (evenOdd % 2 === 0) {
-        alert(`${evenOdd} is even`);
-    }
-    else {
+        alert(`${evenOdd} is even`)
+    } else if (isNaN(evenOdd)) {
+        alert("Please input a number");
+        even();
+    } else {
         alert(`${evenOdd} is odd`)
     }
 }

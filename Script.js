d23524b06d3de2bd8591438b30a91fadcc7d7aef
MrSkidood <sbotnari6@gmail.com>
2023-01-25 19:38:11 +0200

    learned how to do nested tenary operators

--- Script.js
+++ Script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+// let inputNum = prompt("number");
+// if (inputNum > 0) {
+//     alert("1")
+// } else if (inputNum === 0) {
+//     alert("0")
+// } else {
+//     alert("-1")
+// }
+let outputNum = prompt("number");
+outputNum > 0 ? alert("1") : outputNum == 0 ? alert("0") : alert("-1");

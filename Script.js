182819dbcdf785b0e5249fdd5a111e21cdc6a95a
stefan botnari <theblindfold123@gmail.com>
2023-01-25 11:14:01 +0200

    added a function that check whether the number given is odd or even

--- Script.js
+++ Script.js
@@ -1,1 +1,11 @@
 "use strict";
+even();
+function even() {
+    let evenOdd = prompt("type a number");
+    if (evenOdd % 2 === 0) {
+        alert(`${evenOdd} is even`);
+    }
+    else {
+        alert(`${evenOdd} is odd`)
+    }
+}
